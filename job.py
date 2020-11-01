7164c827c6a51526cf9d3e19a3686d26daaf68d2
Eric <ericaltendorf@gmail.com>
2020-10-31 18:40:27 -0700

    First fully functional version.
    Note: there seems to be some deadlock that sometimes happens
    when initializing the list of active jobs and their files.

--- job.py
+++ job.py
@@ -11,44 +11,169 @@ import os
 import re
 import threading
 import time
+import psutil      # apt-get install python-psutil
 import random
 import sys
-
-class JobStatus(Enum):
-    UNSTARTED = auto()
-    RUNNING = auto()
-    SUSPENDED = auto()
-    CANCELING = auto()
-    CANCELED = auto()
-    COMPLETED = auto()
 
 class Job:
     'Represents a plotter job'
     started = False
+    k = 0
+    r = 0
+    u = 0
+    b = 0
+    n = 0  # probably not used
     tmpdir = ''
     tmp2dir = ''
     dstdir = ''
     logfile = ''
     jobfile = ''
-    status = JobStatus.UNSTARTED
-    status_note = ''  # extra info as to why status (e.g. suspended) was entered
     job_id = 0
     plot_id = 0
-    proc = None   # will get a subprocess
+    proc = None   # will get a psutil.Process
 
-    def __init__(self, tmpdir, tmp2dir, dstdir):
-        self.tmpdir = tmpdir
-        self.tmp2dir = tmp2dir
-        self.dstdir = dstdir
-        status = JobStatus.UNSTARTED
-        self.job_id = hex(random.getrandbits(32))
+    def get_running_jobs(logroot):
+        jobs = []
+        for proc in psutil.process_iter(['pid', 'name']):
+            if proc.name() == 'chia':
+                args = proc.cmdline()
+                # args[0]=python, args[1]=chia
+                if args[2] == 'plots' and args[3] == 'create':
+                    jobs += [ Job(proc, logroot) ]
+        return jobs
 
-    def status_str(self):
-        return '%s pid:%s %s (%s): %s, %s, %s' % (self.job_id, self.proc.pid, self.status, self.status_note, self.tmpdir, self.tmp2dir, self.dstdir)
+    def __init__(self, proc, logroot):
+        '''Initialize from an existing psutil.Process object.  must know logroot in order to understand open files'''
+        self.proc = proc
+
+        with self.proc.oneshot():
+
+            print('Initializing Job object for chia process %d' % self.proc.pid)
+
+            # Parse command line args
+            args = self.proc.cmdline()
+            assert len(args) > 4
+            assert 'python' in args[0]
+            assert 'chia' in args[1]
+            assert 'plots' == args[2]
+            assert 'create' == args[3]
+            for i in range(4, len(args), 2):
+                arg = args[i]
+                val = args[i + 1]
+                if arg == '-k':
+                    self.k = val
+                elif arg == '-r':
+                    self.r = val
+                elif arg == '-b':
+                    self.b = val
+                elif arg == '-u':
+                    self.u = val
+                elif arg == '-t':
+                    self.tmpdir = val
+                elif arg == '-2':
+                    self.tmp2dir = val
+                elif arg == '-d':
+                    self.dstdir = val
+                elif arg == '-n':
+                    self.n = val
+                else:
+                    print('Warning: unrecognized args: %s %s' % (arg, val))
+
+            # Find logfile.  May be open more than once.
+            print('finding logfile')
+            for f in self.proc.open_files():
+                print('looking at %s' % f.path)
+                if logroot in f.path:
+                    print('looks like a logfile')
+                    if self.logfile:
+                        assert self.logfile == f.path
+                    else:
+                        self.logfile = f.path
+                    break
+
+            # Find plot ID.
+            print('finding plot ID from log file %s' % self.logfile)
+            assert self.logfile
+            with open(self.logfile, 'r') as f:
+                for line in f:
+                    m = re.match('^ID: ([0-9a-f]*)', line)
+                    if m:
+                        self.plot_id = m.group(1)
+                        break
+
+    def progress(self):
+        assert self.logfile
+
+        # Map from phase number to step number reached in that phase.
+        # Phase 1 steps are <started>, table1, table2, ...
+        # Phase 2 steps are <started>, table7, table6, ...
+        # Phase 3 steps are <started>, tables1&2, tables2&3, ...
+        # Phase 4 steps are <started>
+        phase_steps = {}
+
+        with open(self.logfile, 'r') as f:
+            for line in f:
+                # "Starting phase 1/4: Forward Propagation into tmp files... Sat Oct 31 11:27:04 2020"
+                m = re.search(r'^Starting phase (\d).*', line)
+                if m:
+                    phase = int(m.group(1))
+                    phase_steps[phase] = 0
+
+                # Phase 1: "Computing table 2"
+                m = re.search(r'^Computing table (\d).*', line)
+                if m:
+                    phase_steps[1] = max(phase_steps[1], int(m.group(1)))
+
+                # Phase 2: "Backpropagating on table 2"
+                m = re.search(r'^Backpropagating on table (\d).*', line)
+                if m:
+                    phase_steps[2] = max(phase_steps[2], 7 - int(m.group(1)))
+
+                # Phase 3: "Compressing tables 4 and 5"
+                m = re.search(r'^Compressing tables (\d) and (\d).*', line)
+                if m:
+                    phase_steps[3] = max(phase_steps[3], int(m.group(1)))
+
+                # TODO also collect timing info:
+
+                # "Time for phase 1 = 22796.7 seconds. CPU (98%) Tue Sep 29 17:57:19 2020"
+                # for phase in ['1', '2', '3', '4']:
+                    # m = re.search(r'^Time for phase ' + phase + ' = (\d+.\d+) seconds..*', line)
+                    # if m:
+                        # data.setdefault(key, {}).setdefault('phase ' + phase, []).append(float(m.group(1)))
+
+                # Total time = 49487.1 seconds. CPU (97.26%) Wed Sep 30 01:22:10 2020
+                # m = re.search(r'^Total time = (\d+.\d+) seconds.*', line)
+                # if m:
+                    # data.setdefault(key, {}).setdefault('total time', []).append(float(m.group(1)))
+
+        phase = max(phase_steps.keys())
+        step = phase_steps[phase]
+            
+        return (phase, step)
+
+    def status_str_short(self):
+        return 'pid:{pid} tmp:{tmp}, {tmp2} dst:{dst} ID:{plotid}'.format(
+            pid = self.proc.pid,
+            tmp = self.tmpdir,
+            tmp2 = self.tmp2dir,
+            dst = self.dstdir,
+            plotid = self.plot_id
+            )
+
+    def status_str_long(self):
+        return '{plot_id}\npid:{pid}\ntmp:{tmp}\ntmp2:{tmp2}\ndst:{dst}\nlogfile:{logfile}'.format(
+            plot_id = self.plot_id,
+            pid = self.proc.pid,
+            tmp = self.tmpdir,
+            tmp2 = self.tmp2dir,
+            dst = self.dstdir,
+            plotid = self.plot_id,
+            logfile = self.logfile
+            )
 
     def get_mem_usage(self):
-        # resource.getrusage()  # TODO this isn't right, need more research
-        return 0
+        return self.proc.memory_info().vms  # Total, inc swapped
 
     def get_tmp_usage(self):
         total_bytes = 0
@@ -58,60 +183,40 @@ class Job:
                     total_bytes += entry.stat().st_size
         return total_bytes
 
-    def start(self, logdir):
-        check_status(JobStatus.UNSTARTED) || return -1;
+    def get_run_status(self):
+        '''Running, suspended, etc.'''
+        return self.proc.status()
 
-        job_fname_base = datetime.now().strftime('%Y-%m-%d-%H-%M') + '.' +
-            os.path.basename(tmpdir) + '.' +
-            self.job_id
+    def get_time_wall(self):
+        # This doesn't seem to be working.  TODO
+        return int(time.time() - self.proc.create_time())
 
-        self.logfile = os.path.join(logdir, '%s.log' % job_fname_base)
-        self.jobfile = os.path.join(logdir, '%s.cmd' % job_fname_base)
+    def get_time_user(self):
+        return int(self.proc.cpu_times().user)
 
-        # params
-        plot_args = ['chia', 'plots', 'create',
-                '-k', str(kval),
-                '-n', str(n_sequential),
-                '-r', str(n_threads),
-                '-u', str(n_buckets),
-                '-b', str(job_buffer),
-                '-t', tmpdir,
-                '-2', tmpdir2,
-                '-d', dstdir
-                ]
+    def get_time_sys(self):
+        return int(self.proc.cpu_times().system)
 
-        # Define process
-        self.proc = subprocess.Popen(['chia'] + plot_args)   # try ', shell=True)' if problems
-
-        # TODO
-        # Fork process
-        # Write job metainfo
-        # Start job
-        self.status = JobStatus.RUNNING
-
-        if not dryrun:
-            with open(self.jobfile, 'w') as f:
-                # TODO: write more of the params etc.
-                f.write('Command executed:\n')
-                f.write(plot_cmd_str + '\n')
-            # Could do this better, I think
-            call(full_cmd, shell=True)
+    def get_time_iowait(self):
+        return int(self.proc.cpu_times().iowait)
 
     def suspend(self, reason=''):
-        # TODO learn about python 'assert'
-        check_status(JobStatus.RUNNING) || return -1;
-        self.proc.send_signal(signal.SIGSTOP)
-        self.status = JobStatus.SUSPENDED
+        self.proc.suspend()
         self.status_note = reason
 
     def resume(self):
-        check_status(JobStatus.SUSPENDED) || return -1;
-        self.proc.send_signal(signal.SIGCONT)
+        self.proc.resume()
+
+    def get_temp_files(self):
+        temp_files = []
+        for f in self.proc.open_files():
+            if self.tmpdir in f.path or self.tmp2dir in f.path or self.dstdir in f.path:
+                temp_files.append(f.path)
+        return temp_files
 
     def cancel(self):
         'Cancel an already running job'
         self.proc.terminate()
-        self.status = JobStatus.CANCELED
 
     def check_status(self, expected_status):
         if (self.status == expected_status):
--- manager.py
+++ manager.py
@@ -2,6 +2,7 @@
 
 # TODO do we use all these?
 from datetime import datetime
+from datetime import timedelta
 from enum import Enum, auto
 from subprocess import call
 import argparse
@@ -12,7 +13,9 @@ import re
 import threading
 import time
 import random
+import readline          # For nice CLI
 import sys
+import texttable as tt   # from somewhere?
 
 # Plotman libraries
 from job import Job
@@ -53,17 +56,17 @@ all_jobs = []
 test_global = 'initial value'
 
  
-def daemon_thread():
-    'Daemon thread for automatically starting jobs and monitoring job status'
-    global all_jobs
-    print('entering thread')
-    while True:
-        print('Plot initiator thread creating job')
-        new_job = Job('/tmp/a', '/tmp/b', test_global)
-        all_jobs = all_jobs + [ new_job ]
-        for job in all_jobs:
-            print(job.status_str())
-        time.sleep(5)
+#def daemon_thread():
+    #'Daemon thread for automatically starting jobs and monitoring job status'
+    #global all_jobs
+    #print('entering thread')
+    #while True:
+        #print('Plot initiator thread creating job')
+        #new_job = Job('/tmp/a', '/tmp/b', test_global)
+        #all_jobs = all_jobs + [ new_job ]
+        #for job in all_jobs:
+            #print(job.status_str_short())
+        #time.sleep(5)
 
 def init_logdir(logdir):
     try:
@@ -75,64 +78,144 @@ def init_logdir(logdir):
         print('Failed to init logdirs: {0}'.format(err))
         sys.exit(1)
 
+def human_format(num, precision):
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return (('%.' + str(precision) + 'f%s') % (num, ['', 'K', 'M', 'G', 'T', 'P'][magnitude]))
+
+def time_format(sec, precision):
+    if sec < 60:
+        return '%ds' % sec
+    else:
+        return '%d:%02d' % (int(sec / 3600), int((sec % 3600) / 60))
+
+def status_report(jobs):
+    tab = tt.Texttable()
+    headings = ['plot id', 'status', 'phase', 'k', 'r', 'tmp dir', 'tmp', 'wall', 'pid', 'mem', 'user', 'sys', 'io']
+    tab.header(headings)
+    for j in jobs:
+        row = [j.plot_id[:8] + '...',
+               j.get_run_status(),
+               '%d:%d' % j.progress(),
+               j.k,
+               j.r,
+               j.tmpdir,
+               human_format(j.get_tmp_usage(), 0),
+               time_format(j.get_time_wall(), 1),
+               j.proc.pid,
+               human_format(j.get_mem_usage(), 1),
+               time_format(j.get_time_user(), 1),
+               time_format(j.get_time_sys(), 1),
+               time_format(j.get_time_iowait(), 1)
+               ]
+        tab.add_row(row)
+
+    (rows, columns) = os.popen('stty size', 'r').read().split()
+    tab.set_max_width(int(columns))
+    tab.set_deco(tt.Texttable.BORDER | tt.Texttable.HEADER )
+    return tab.draw()
+ 
+def select_jobs_by_partial_id(jobs, partial_id):
+    selected = []
+    for j in jobs:
+        if j.plot_id.startswith(partial_id):
+            selected.append(j)
+    return selected
+
 if __name__ == "__main__":
     random.seed()
 
-    # Start plot initiator thread
-    daemon_thread = threading.Thread(target=daemon_thread, args=(), daemon=True)
-    daemon_thread.start()
+    print('...scanning process tables')
+    jobs = Job.get_running_jobs(logroot)
+    print('Welcome to PlotMan.  Detected %d active plot jobs.  Type \'h\' for help.' % len(jobs))
 
+    # This is a really cheapo CLI.  Would be better to use some standard library.
     while True:
-        cmd = input('> ')
-        if cmd:
-            test_global = cmd
-            print(cmd)
+        cmd_line = input('plotman> ')
+        if cmd_line:
+            # Re-read active jobs
+            jobs = Job.get_running_jobs(logroot)
+
+            cmd_line = cmd_line.split()
+            cmd = cmd_line[0]
+            args = cmd_line[1:]
+
+            if cmd == 'h':
+                print('Commands which reference a job require an unambiguous prefix of the plot id')
+                print('  l            : list current jobs')
+                print('  d <idprefix> : show details for a plot job')
+                print('  f <idprefix> : show temp files for a job')
+                print('  k <idprefix> : kill and cleanup a plot job')
+                print('  p <idprefix> : pause (suspend) a plot job')
+                print('  r <idprefix> : resume a plot job')
+                print('  h            : help info (this message)')
+                print('  x            : exit')
+                continue
+
+            elif cmd == 'l':
+                print(status_report(jobs))
+                continue
+
+            elif cmd == 'x':
+                break
+
+            elif cmd == 'd' or cmd == 'f' or cmd == 'k' or cmd == 'p' or cmd == 'r':
+                if (len(args) != 1):
+                    print('Need to supply job id spec')
+                    continue
+
+                id_spec = args[0]
+                selected = select_jobs_by_partial_id(jobs, id_spec)
+                if (len(selected) == 0):
+                    print('Error: %s matched no jobs.')
+                    continue
+                elif len(selected) > 1:
+                    print('Error: "%s" matched multiple jobs:' % id_spec)
+                    for j in selected:
+                        print('  %s' % j.plot_id)
+                    continue
+                else:
+                    job = selected[0]
+
+                    # Do the real command
+                    if cmd == 'd':
+                        print(job.status_str_long())
+
+                    elif cmd == 'f':
+                        temp_files = job.get_temp_files()
+                        for f in temp_files:
+                            print('  %s' % f)
+
+                    elif cmd == 'k':
+                        # First suspend so job doesn't create new files
+                        print('Pausing PID %d, plot id %s' % (job.proc.pid, job.plot_id))
+                        job.suspend()
+
+                        temp_files = job.get_temp_files()
+                        print('Will kill pid %d, plot id %s' % (job.proc.pid, job.plot_id))
+                        print('Will delete %d temp files' % len(temp_files))
+                        conf = input('Are you sure? ("y" to confirm): ')
+                        if (conf != 'y'):
+                            print('canceled.  If you wish to resume the job, do so manually.')
+                        else:
+                            print('killing...')
+                            job.cancel()
+                            print('cleaing up temp files...')
+                            for f in temp_files:
+                                os.remove(f)
+
+                    elif cmd == 'p':
+                        print('Pausing ' + job.plot_id)
+                        job.suspend()
+                    elif cmd == 'r':
+                        print('Resuming ' + job.plot_id)
+                        job.resume()
+
+            else:
+                print('Unknown command: %s' % cmd)
+                continue 
 
     sys.exit(0)
 
-
-if (not dryrun):
-    init_logdir(logdir)
-    # ensure we're in chia env
-    # need to fix thsi one: chia init || exit 1
-
-print('Running %d jobs per temp dir' % n_jobs_per_dir)
-print('Temp dirs: { %s }' % ', '.join(tmpdirs) )
-print('Dest dirs: %s + { %s }' % ( dstdir_root, ', '.join(dstdirs) ))
-print('Logging to ' + logdir)
-
-for tmpdir_idx in range(len(tmpdirs)):
-    tmpdir = tmpdirs[tmpdir_idx]
-
-    for job_idx in range(n_jobs_per_dir):
-        delay = job_idx * job_stagger_s + tmpdir_idx * tmpdir_stagger_s
-
-        dst_idx = (tmpdir_idx * n_jobs_per_dir + job_idx) % len(dstdirs)
-
-        dstdir = os.path.join(dstdir_root, dstdirs[dst_idx])
-
-
-# move to job.py:
-        job_fname_base = ('tmp%02d-job%02d' % (tmpdir_idx, job_idx))
-        logfile = os.path.join(logdir, '%s.log' % job_fname_base)
-        cmdfile = os.path.join(logdir, '%s.cmd' % job_fname_base)
-
-        plot_args = ['chia', 'plots', 'create',
-                '-k', str(kval),
-                '-n', str(n_sequential),
-                '-r', str(n_threads),
-                '-u', str(n_buckets),
-                '-b', str(job_buffer),
-                '-t', tmpdir,
-                '-2', tmpdir2,
-                '-d', dstdir
-                ]
-
-        plot_cmd_str = ' '.join(plot_args)
-        full_cmd = 'sleep %d && %s > %s 2>&1 &' % (delay, plot_cmd_str, logfile)
-        print('tmpdir %d job %d: %s' % (tmpdir_idx, job_idx, full_cmd))
-        if not dryrun:
-            with open(cmdfile, 'w') as f:
-                f.write(plot_cmd_str + '\n')
-            # Could do this better, I think
-            call(full_cmd, shell=True)
