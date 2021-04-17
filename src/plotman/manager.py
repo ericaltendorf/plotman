f824ebd45b5b25b078838153fb0395da6a3a9f87
Mike White <mike@mikehw.com>
2021-04-17 10:28:46 -0500

    Consider in-flight plots

--- src/plotman/manager.py
+++ src/plotman/manager.py
@@ -85,9 +85,9 @@ def maybe_start_new_plot(dir_cfg, sched_cfg, plotting_cfg):
         rankable = [ (d, phases[0]) if phases else (d, (999, 999))
                 for (d, phases) in eligible ]
         dir2ph = {d: ph for (d, ph) in dstdirs_to_youngest_phase(jobs).items()
-                  if (d in dir_cfg.dst and plot_util.is_valid_plot_dst(d, sched_cfg))}
+                  if (d in dir_cfg.dst and plot_util.is_valid_plot_dst(d, sched_cfg, jobs))}
         unused_dirs = [d for d in dir_cfg.dst
-                       if d not in dir2ph.keys() and plot_util.is_valid_plot_dst(d, sched_cfg)]
+                       if d not in dir2ph.keys() and plot_util.is_valid_plot_dst(d, sched_cfg, jobs)]
 
         if not unused_dirs and not dir2ph:
             wait_reason = 'no eligible dstdirs'
