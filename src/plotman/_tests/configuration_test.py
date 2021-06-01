aca45997ab86190f33d6edf6066adb6408698713
Kyle Altendorf <sda@fstab.net>
2021-05-31 23:05:31 -0400

    remove tests no longer needed due to config defaults

--- src/plotman/_tests/configuration_test.py
+++ src/plotman/_tests/configuration_test.py
@@ -18,22 +18,6 @@ def test_get_validated_configs__default(config_text):
     res = configuration.get_validated_configs(config_text, '')
     assert isinstance(res, configuration.PlotmanConfig)
 
-def test_tool_interactive_should_be_none(config_text):
-    """When tools/interacting doesn't exist, its configuration property should be None"""
-    loaded_yaml = yaml.load(config_text, Loader=yaml.SafeLoader)
-    
-    del loaded_yaml["tools"]["interactive"]
-    reloaded_yaml = configuration.get_validated_configs(yaml.dump(loaded_yaml, Dumper=yaml.SafeDumper), '')
-    assert reloaded_yaml.tools.interactive is None
-
-def test_tool_section_does_not_exist(config_text):
-    """When tools doesn't exist, its configuration property should be None"""
-    loaded_yaml = yaml.load(config_text, Loader=yaml.SafeLoader)
-    
-    del loaded_yaml["tools"]
-    reloaded_yaml = configuration.get_validated_configs(yaml.dump(loaded_yaml, Dumper=yaml.SafeDumper), '')
-    assert reloaded_yaml.tools is None
-
 def test_get_validated_configs__malformed(config_text):
     """Check that get_validated_configs() raises exception with invalid plotman.yaml contents."""
     loaded_yaml = yaml.load(config_text, Loader=yaml.SafeLoader)
