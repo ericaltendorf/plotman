d788db432d3ab86135803c2e84a413c7592cb0cb
spence.copper@gmail.com <chris@Chriss-MBP.localdomain>
2021-06-05 21:30:33 -0600

    fixing yaml

--- src/plotman/configuration.py
+++ src/plotman/configuration.py
@@ -79,6 +79,7 @@ class Scheduling:
     tmpdir_stagger_phase_major: int
     tmpdir_stagger_phase_minor: int
     tmpdir_stagger_phase_limit: int = 1  # If not explicit, "tmpdir_stagger_phase_limit" will default to 1
+    dst_dir_with_most_free_space: bool
 
 @attr.frozen
 class Plotting:
