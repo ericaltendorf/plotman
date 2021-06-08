15663757531f5154c4c484fbb3bfb72c00267ce3
racergoodwin <84841970+racergoodwin@users.noreply.github.com>
2021-06-08 16:58:07 +0100

    Update configuration.py
    Changes for reading the tmpdir overrides

--- src/plotman/configuration.py
+++ src/plotman/configuration.py
@@ -186,6 +186,9 @@ class Archiving:
 
 @attr.frozen
 class TmpOverrides:
+    tmpdir_stagger_phase_major: Optional[int] = None
+    tmpdir_stagger_phase_minor: Optional[int] = None
+    tmpdir_stagger_phase_limit: Optional[int] = None
     tmpdir_max_jobs: Optional[int] = None
 
 @attr.frozen
@@ -222,7 +225,6 @@ class Directories:
     tmp: List[str]
     dst: Optional[List[str]] = None
     tmp2: Optional[str] = None
-    tmp_overrides: Optional[Dict[str, TmpOverrides]] = None
 
     def dst_is_tmp(self):
         return self.dst is None and self.tmp2 is None
@@ -250,6 +252,7 @@ class Scheduling:
     tmpdir_stagger_phase_major: int
     tmpdir_stagger_phase_minor: int
     tmpdir_stagger_phase_limit: int = 1  # If not explicit, "tmpdir_stagger_phase_limit" will default to 1
+    tmp_overrides: Optional[Dict[str, TmpOverrides]] = None
 
 @attr.frozen
 class Plotting:
