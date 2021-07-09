40d45071cbd0975025319d88d33af23584b18a8e
Leonardo Leite <84483790+roxleopardo@users.noreply.github.com>
2021-07-09 01:28:07 -0300

    Update plot_util.py

--- src/plotman/plot_util.py
+++ src/plotman/plot_util.py
@@ -17,6 +17,9 @@ def df_b(d: str) -> int:
 def get_plotsize(k: int) -> int:
     return (int)(_get_plotsize_scaler(k) * k * pow(2, k))
 
+def get_k32_plotsize():
+    return 108 * GB
+
 def is_valid_plot_dst(d, sched_cfg, all_jobs):
     if sched_cfg.stop_when_dst_full:
         space = df_b(d)
@@ -24,7 +27,7 @@ def is_valid_plot_dst(d, sched_cfg, all_jobs):
         # Note: This is underestimates the free space available when a
         #       job is in phase 4 since the plot is partially moved to dst,
         #       once phase 4 is complete a new plot will eventually kick off
-        jobs_to_dstdir = job.job_phases_for_dstdir(d, all_jobs)
+        jobs_to_dstdir = plotman.job.job_phases_for_dstdir(d, all_jobs)
         space -= len(jobs_to_dstdir) * get_k32_plotsize()
         return enough_space_for_k32(space)
     return True
