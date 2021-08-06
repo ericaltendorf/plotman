7111a66f91be69719da25af0470a22e082129d35
Artanicus <artanicus@nocturnal.fi>
2021-08-06 12:21:19 +0300

    Update Chia reference, use :latest tag
    Also:
    - Don't try to delete image, most registries don't support deletion
    - Don't override UID and instead use bUID, it's a system env variable and can have odd
    consequences

--- build-docker-plotman.sh
+++ build-docker-plotman.sh
@@ -2,22 +2,20 @@
 
 DOCKER_REGISTRY="<docker-registry>"
 PROJECT="chia-plotman"
-TAG="plotter"
+TAG="latest"
 BASE_CONTAINER="ubuntu:20.04"
-CHIA_GIT_REFERENCE="1.1.7"
+CHIA_GIT_REFERENCE="1.2.3"
 
 # The UID/GID should match the 'chia' owner of the directories on the host system
-UID=10001
-GID=10001
-
-docker rmi ${LOCAL_REGISTRY}/${PROJECT}:${TAG}
+bUID=10001
+bGID=10001
 
 docker build . \
 	--squash \
 	--build-arg BASE_CONTAINER=${BASE_CONTAINER} \
 	--build-arg CHIA_GIT_REFERENCE=${CHIA_GIT_REFERENCE} \
-	--build-arg UID=${UID} \
-	--build-arg GID=${GID} \
+	--build-arg UID=${bUID} \
+	--build-arg GID=${bGID} \
 	-f docker/Dockerfile \
         -t ${DOCKER_REGISTRY}/${PROJECT}:${TAG}
 
