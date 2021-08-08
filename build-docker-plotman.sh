0ea2dc87722b58aeabac9b1964caa0c97e3abb74
Artanicus <artanicus@nocturnal.fi>
2021-08-08 16:55:29 +0300

    Use more descriptive variables for build-time UID & GID

--- build-docker-plotman.sh
+++ build-docker-plotman.sh
@@ -7,15 +7,15 @@ BASE_CONTAINER="ubuntu:20.04"
 CHIA_GIT_REFERENCE="1.2.3"
 
 # The UID/GID should match the 'chia' owner of the directories on the host system
-bUID=10001
-bGID=10001
+build_UID=10001
+build_GID=10001
 
 docker build . \
 	--squash \
 	--build-arg BASE_CONTAINER=${BASE_CONTAINER} \
 	--build-arg CHIA_GIT_REFERENCE=${CHIA_GIT_REFERENCE} \
-	--build-arg UID=${bUID} \
-	--build-arg GID=${bGID} \
+	--build-arg UID=${build_UID} \
+	--build-arg GID=${build_GID} \
 	-f docker/Dockerfile \
         -t ${DOCKER_REGISTRY}/${PROJECT}:${TAG}
 
